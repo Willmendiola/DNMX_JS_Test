a8ca4440fbdd33fff827d8b11d0325b70020d614
lrodriguez119416 <105320879+lrodriguez119416@users.noreply.github.com>
2022-11-24 16:05:29 -0600

    Update object.js

--- object.js
+++ object.js
@@ -24,11 +24,3 @@ let usuarios = [
 // Realizar una funcion, o utilizar una funcion nativa de JS la cual me permita imprimir el nombre del usuario con la edad menor
 // Realizar una funcion, o utilizar una funcion nativa de JS la cual me permita imprimir el nombre del usuario con la estatura mayor
 // Realizar una funcion, o utilizar una funcion nativa de JS la cual me permita imprimir el nombre del usuario con la estatura menor
-let user_mayor = usuarios.reduce((res, obj) => { return obj.edad < res.edad ? obj: res; });
-console.log("El usuario de menor edad es:" + user_mayor.nombre);
-let user_menor = usuarios.reduce((res, obj) => { return obj.edad > res.edad ? obj: res; });
-console.log("El usuario de mayor edad es:" + user_menor.nombre);
-let user_alto = usuarios.reduce((res, obj) => { return obj.estatura < res.estatura ? obj: res; });
-console.log("El usuario de menor estatura es:" + user_alto.nombre);
-let user_bajo = usuarios.reduce((res, obj) => { return obj.estatura > res.estatura ? obj: res; });
-console.log("El usuario mas alto es:" + user_bajo.nombre);
