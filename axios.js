54ad4eafd57a11307d8c5621dd49cc91ae28fdcc
lr119416 <lr119416@gmail.com>
2022-11-24 08:20:52 -0600

    Initial commit

---
+++ axios.js
@@ -0,0 +1,2 @@
+// Crear una peticion axios de manera que me permita descargar el contenido de una solicitud GET
+// por ejemplo realizar una petición get a la pagina de google https://www.google.com/
