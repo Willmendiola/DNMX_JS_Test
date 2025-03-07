f96f90a1ab558bbc902c08a9ccf3e91573c80e08
Willmendiola <william777caudillo@gmail.com>
2025-03-07 09:27:00 -0600

    Update recursive.js

--- recursive.js
+++ recursive.js
@@ -11,3 +11,19 @@
 -##########
 ########### 
 */
+
+function imprimirTriangulo(filas, i = 1) {
+  if (i > filas) return; // Condición de salida de la recursión
+
+  // Crear la línea combinando los espacios y los '#'
+  let linea = ' '.repeat(filas - i) + '#'.repeat(i);
+
+  // Imprimir la línea
+  console.log(linea);
+
+  // Llamada recursiva para la siguiente fila
+  imprimirTriangulo(filas, i + 1);
+}
+
+// Llamar a la función con el número de filas
+imprimirTriangulo(11);
